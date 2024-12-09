7f909f9c4aec597766864856d343b0c9119498e9
tenado <690784374@qq.com>
2024-12-09 22:56:35 +0800

    chore: 调整root变量生成逻辑，不再放在body上，改为放在head里面

--- examples/theme.js
+++ examples/theme.js
@@ -1,7 +1,18 @@
 import { theme2Variable } from "../src/utils/theme.js";
-export default (style) => {
-  const variables = theme2Variable(style);
+export default (baseStyle) => {
+  const variables = theme2Variable(baseStyle);
+  let styleContent = "";
   for (let variable in variables) {
-    document.body.style.setProperty(variable, variables[variable]);
+    styleContent += `${variable}: ${variables[variable]};`;
+  }
+  styleContent = `:root { ${styleContent} }`;
+  let style = document.getElementById("theme");
+  if (!style) {
+    style = document.createElement("style");
+    style.id = "theme";
+    style.innerHTML = styleContent;
+    document.head.appendChild(style);
+  } else {
+    style.innerHTML = styleContent;
   }
 };
