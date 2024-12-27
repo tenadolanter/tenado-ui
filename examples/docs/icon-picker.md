6567efa98ca2308a2829570fcf17a685f011d030
tenado <690784374@qq.com>
2024-12-27 18:28:36 +0800

    chore: icon-picker组件添加

--- examples/docs/icon-picker.md
+++ examples/docs/icon-picker.md
@@ -8,7 +8,12 @@
 
 ```html
 <template>
-  <td-icon-picker v-model="value" placeholder="请选择"></td-icon-picker>
+  <td-icon-picker
+    style="width: 100%"
+    v-model="value"
+    :options="options"
+    placeholder="请选择"
+  ></td-icon-picker>
 </template>
 
 <script>
@@ -16,6 +21,12 @@
     data() {
       return {
         value: "",
+        options: this.$icon.map((item) => {
+          return {
+            value: item.fontClass,
+            label: item.fontClass,
+          };
+        }),
       };
     },
   };
@@ -23,3 +34,101 @@
 ```
 
 :::
+
+### 禁用状态
+
+选择器不可用状态
+
+:::demo 为`td-icon-picker`设置`disabled`属性，则整个选择器不可用
+
+```html
+<template>
+  <td-icon-picker
+    v-model="value"
+    :options="options"
+    disabled
+    placeholder="请选择"
+  ></td-icon-picker>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value: "",
+        options: this.$icon.map((item) => {
+          return {
+            value: item.fontClass,
+            label: item.fontClass,
+          };
+        }),
+      };
+    },
+  };
+</script>
+```
+
+:::
+
+### 可清空单选
+
+包含清空按钮，可将选择器清空为初始状态
+
+:::demo 为`td-icon-picker`设置`clearable`属性，则可将选择器清空。需要注意的是，`clearable`属性仅适用于单选。
+
+```html
+<template>
+  <td-icon-picker
+    v-model="value"
+    :options="options"
+    clearable
+    placeholder="请选择"
+  ></td-icon-picker>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        value: "",
+        options: this.$icon.map((item) => {
+          return {
+            value: item.fontClass,
+            label: item.fontClass,
+          };
+        }),
+      };
+    },
+  };
+</script>
+```
+
+:::
+
+### Attributes
+
+| 参数               | 说明                         | 类型                    | 可选值            | 默认值 |
+| ------------------ | ---------------------------- | ----------------------- | ----------------- | ------ |
+| value / v-model    | 绑定值                       | string                  | —                 | —      |
+| placeholder        | 是否多选                     | boolean                 | —                 | —      |
+| size               | 输入框尺寸                   | string                  | medium/small/mini | —      |
+| disabled           | 是否禁用                     | boolean                 | —                 | false  |
+| clearable          | 是否可以清空选项             | boolean                 | —                 | false  |
+| popperClass        | 下拉框的类名                 | string                  | —                 | —      |
+| popperAppendToBody | 是否将弹出框插入至 body 元素 | boolean                 | —                 | false  |
+| options            | 可以被选择的值               | Array&lt;OptionItem&gt; | —                 | []     |
+
+### OptionItem
+
+| 参数  | 说明                                      | 类型   | 可选值 | 默认值 |
+| ----- | ----------------------------------------- | ------ | ------ | ------ |
+| value | 选项的值                                  | string | —      | —      |
+| label | 选项的标签，若不设置则默认与 `value` 相同 | string | —      | —      |
+
+### Select Events
+
+| 事件名称       | 说明                                     | 回调参数                      |
+| -------------- | ---------------------------------------- | ----------------------------- |
+| change         | 选中值发生变化时触发                     | 目前的选中值                  |
+| visible-change | 下拉框出现/隐藏时触发                    | 出现则为 true，隐藏则为 false |
+| clear          | 可清空的单选模式下用户点击清空按钮时触发 | —                             |
--- packages/icon-picker/src/picker.vue
+++ packages/icon-picker/src/picker.vue
@@ -1,14 +1,102 @@
 <template>
-  <div class="td-icon-picker">
-    <div class="td-icon-picker__inner">
-      <div class="td-icon-picker--placeholder">{{ placeholder }}</div>
-    </div>
+  <div
+    class="td-icon-picker"
+    :class="[iconPickerSize ? 'td-icon-picker--' + iconPickerSize : '']"
+    @click.stop="toggleMenu"
+    v-clickoutside="handleClose"
+  >
+    <td-input
+      :value="value"
+      ref="reference"
+      type="text"
+      :disabled="iconPickerDisabled"
+      :class="[iconPickerSize ? `is-${iconPickerSize}` : '']"
+      :placeholder="placeholder"
+      :readonly="true"
+      @focus="handleFocus"
+      @blur="handleBlur"
+    >
+      <template slot="prepend">
+        <i v-if="value" :class="value"></i>
+      </template>
+      <template slot="suffix">
+        <i
+          v-show="!showClose"
+          :class="[
+            'td-select__caret',
+            'td-input__icon',
+            'td-icon-' + iconClass,
+          ]"
+        ></i>
+        <i
+          v-if="showClose"
+          class="td-select__caret td-input__icon td-icon-circle-close"
+          @click="handleClearClick"
+        ></i>
+      </template>
+    </td-input>
+    <transition
+      name="td-zoom-in-top"
+      @before-enter="handleMenuEnter"
+      @after-leave="doDestroy"
+    >
+      <TdIconPickerDropdown
+        ref="popper"
+        :append-to-body="popperAppendToBody"
+        v-show="visible"
+      >
+        <td-scrollbar
+          tag="ul"
+          wrap-class="td-icon-picker-dropdown__wrap"
+          view-class="td-icon-picker-dropdown__list"
+          ref="scrollbar"
+          v-show="options.length > 0"
+        >
+          <slot>
+            <div class="td-icon-picker-dropdown__icon-wrap">
+              <div
+                class="td-icon-picker-dropdown__icon-item"
+                :class="{ selected: item.value === value }"
+                v-for="item in options"
+                :key="item.value"
+                @click.stop="handlerIconSelect(item)"
+              >
+                <i :class="item.value"></i>
+              </div>
+            </div>
+          </slot>
+        </td-scrollbar>
+      </TdIconPickerDropdown>
+    </transition>
   </div>
 </template>
 
 <script>
+import Emitter from "@tenado/ui/src/mixins/emitter";
+import Focus from "@tenado/ui/src/mixins/focus";
+import Locale from "@tenado/ui/src/mixins/locale";
+import Clickoutside from "@tenado/ui/src/utils/clickoutside";
+import { valueEquals } from "@tenado/ui/src/utils/util";
+import TdInput from "@tenado/ui/packages/input";
+import TdScrollbar from "@tenado/ui/packages/scrollbar";
+import TdIconPickerDropdown from "./picker-dropdown.vue";
 export default {
   name: "TdIconPicker",
+  mixins: [Emitter, Locale, Focus("reference")],
+  inject: {
+    elForm: {
+      default: "",
+    },
+    elFormItem: {
+      default: "",
+    },
+  },
+  directives: { Clickoutside },
+  components: {
+    TdInput,
+    TdScrollbar,
+    TdIconPickerDropdown,
+  },
   props: {
     value: {
       required: true,
@@ -16,7 +104,105 @@ export default {
     placeholder: {
       type: String,
       required: false,
-      default: "请选择",
+      default: () => this.t("el.select.placeholder"),
+    },
+    size: String,
+    disabled: Boolean,
+    clearable: Boolean,
+    popperClass: String,
+    popperAppendToBody: {
+      type: Boolean,
+      default: true,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    _elFormItemSize() {
+      return (this.elFormItem || {}).elFormItemSize;
+    },
+    iconPickerSize() {
+      return this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+    },
+    iconPickerDisabled() {
+      return this.disabled || (this.elForm || {}).disabled;
+    },
+    showClose() {
+      return this.value !== "" && !this.iconPickerDisabled && this.clearable;
+    },
+    iconClass() {
+      return this.visible ? "arrow-up is-reverse" : "arrow-up";
+    },
+  },
+  data() {
+    return {
+      visible: false,
+      softFocus: false,
+      menuVisibleOnFocus: false,
+    };
+  },
+  watch: {
+    value(val, oldVal) {
+      if (!valueEquals(val, oldVal)) {
+        this.dispatch("TdFormItem", "el.form.change", val);
+      }
+    },
+    visible(val) {
+      this.$emit("visible-change", val);
+    },
+  },
+  methods: {
+    toggleMenu() {
+      if (!this.iconPickerDisabled) {
+        if (this.menuVisibleOnFocus) {
+          this.menuVisibleOnFocus = false;
+        } else {
+          this.visible = !this.visible;
+        }
+        if (this.visible) {
+          (this.$refs.input || this.$refs.reference).focus();
+        }
+      }
+    },
+    handleClose() {
+      this.visible = false;
+    },
+    handleFocus(event) {
+      if (!this.softFocus) {
+        this.$emit("focus", event);
+      } else {
+        this.softFocus = false;
+      }
+    },
+    handleBlur() {
+      this.softFocus = false;
+    },
+    handleMenuEnter() {},
+    doDestroy() {
+      this.$refs.popper && this.$refs.popper.doDestroy();
+    },
+    emitChange(val) {
+      if (!valueEquals(this.value, val)) {
+        this.$emit("change", val);
+      }
+    },
+    handlerIconSelect(item) {
+      this.$emit("input", item.value);
+      this.emitChange(item.value);
+      this.visible = false;
+    },
+    deleteSelected(event) {
+      event.stopPropagation();
+      const value = "";
+      this.$emit("input", value);
+      this.emitChange(value);
+      this.visible = false;
+      this.$emit("clear");
+    },
+    handleClearClick(event) {
+      this.deleteSelected(event);
     },
   },
 };
